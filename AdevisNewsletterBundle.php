d7b05dd13f91d3385bae3cc32586b8d43dec53ee
miky <mathias89100@gmail.com>
2017-04-08 23:04:33 +0200

    Merge branch 'master' of github.com:mikyprog/AdminBundle
    # Veuillez entrer un message de validation pour expliquer en quoi cette fusion est
    # nécessaire, surtout si cela fusionne une branche amont mise à jour dans une branche de sujet.
    #
    # Les lignes commençant par '#' seront ignorées, et un message vide
    # abandonne la validation.

--- AdevisNewsletterBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Miky\Bundle\NewsletterBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class MikyNewsletterBundle extends Bundle
-{
-}
--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -18,7 +18,7 @@ class Configuration implements ConfigurationInterface
     public function getConfigTreeBuilder()
     {
         $treeBuilder = new TreeBuilder();
-        $rootNode = $treeBuilder->root('adevis_newsletter');
+        $rootNode = $treeBuilder->root('miky_newsletter');
 
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
